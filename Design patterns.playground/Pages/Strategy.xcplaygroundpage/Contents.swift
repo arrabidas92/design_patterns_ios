f9c748e595e3dbe8eac923b5c1fcd3861078916c
Alexandre DUARTE <alexandre.jose.duarte@gmail.com>
2022-01-16 17:47:55 +0100

    add strategy pattern

---
+++ Design patterns.playground/Pages/Strategy.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+import Cocoa
+
+/**
+ The strategy pattern is used to create an interchangeable family of algorithms from which the required process is chosen at run-time.
+ It's an entry point to composition versus inheritance.
+ When you feel that the inheritance hierarchy is not only going downards but also horizontally, you may have to choose composition over inheritance.
+ */
+
+protocol FillableStrategy {
+    func fillUp()
+}
+
+struct DieselStrategy: FillableStrategy {
+    func fillUp() {
+        print("\(DieselStrategy.self) engine must be filled up with diesel")
+    }
+}
+
+struct ElectricStrategy: FillableStrategy {
+    func fillUp() {
+        print("\(ElectricStrategy.self) engine must be filled up with electricity")
+    }
+}
+
+struct GasolineStrategy: FillableStrategy {
+    func fillUp() {
+        print("\(GasolineStrategy.self) engine must be filled up with gasoline")
+    }
+}
+
+struct Client {
+    private let strategy: FillableStrategy
+    
+    init(strategy: FillableStrategy) {
+        self.strategy = strategy
+    }
+    
+    func fillUp() {
+        strategy.fillUp()
+    }
+}
+
+let clientA = Client(strategy: DieselStrategy())
+clientA.fillUp()
+
+let clientB = Client(strategy: ElectricStrategy())
+clientB.fillUp()
+
+let clientC = Client(strategy: GasolineStrategy())
+clientC.fillUp()
+
